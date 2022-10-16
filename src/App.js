3457993e38b080b7e35ca5428043ea9c06e36498
Aayush Senapati <aayushsenapati2002@gmail.com>
2022-10-16 16:11:02 +0530

    Spotify working backup

--- src/App.js
+++ src/App.js
@@ -1,9 +1,26 @@
 import logo from './logo.svg';
 import './App.css';
+import {useEffect,useState} from "react"
 
 function App() {
+  const CLIENT_ID="54c6d6c8a6f347ba9c4d03f1d5be8f3c"
+  const REDIRECT_URI="http://localhost:3000"
+  const AUTH_ENDPOINT="https://accounts.spotify.com/authorize"
+  const RESPONSE_TYPE="token"
+  const [token,setToken]=useState("")
+
+  useEffect(()=>{
+    const hash=window.location.hash
+
+  },[])
+  
+
   return (
-    <p id="p1">hello world</p>
+    <div class="App">
+      <h1 id="p1">Modia</h1>
+      <a href={`${AUTH_ENDPOINT}?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=${RESPONSE_TYPE}`}>Login to Spotify</a>
+
+    </div>
   );
 }
 
