920cb1d738658812d6e96a330f7bf2d7b19f844a
Aayush Senapati <aayushsenapati2002@gmail.com>
2022-10-16 17:27:29 +0530

    token local storage

--- src/App.js
+++ src/App.js
@@ -11,6 +11,15 @@ function App() {
 
   useEffect(()=>{
     const hash=window.location.hash
+    let token=window.localStorage.getItem("token")
+
+    if(!token&&hash){
+      token=hash.substring(1).split("&").find(elem=>elem.startsWith("access_token")).split("=")[1]
+      window.location.hash=""
+      console.log(token)
+      window.localStorage.setItem("token",token)
+      setToken(token)
+    }
 
   },[])
   
@@ -18,7 +27,8 @@ function App() {
   return (
     <div class="App">
       <h1 id="p1">Modia</h1>
-      <a href={`${AUTH_ENDPOINT}?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=${RESPONSE_TYPE}`}>Login to Spotify</a>
+      {!token?<a href={`${AUTH_ENDPOINT}?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=${RESPONSE_TYPE}`}>Login to Spotify</a>:<button>Logout</button>}
+      
 
     </div>
   );
