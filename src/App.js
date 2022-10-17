544bfdba339c130955b7c78902d88c42d182464a
Aayush Senapati <aayushsenapati2002@gmail.com>
2022-10-17 08:40:07 +0530

    color effects added

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import logo from './logo.svg';
 import './App.css';
-import {useEffect,useState} from "react"
+import {useEffect,useState,useRef,useLayoutEffect} from "react"
+
+
 
 function App() {
   const CLIENT_ID="54c6d6c8a6f347ba9c4d03f1d5be8f3c"
@@ -8,6 +10,38 @@ function App() {
   const AUTH_ENDPOINT="https://accounts.spotify.com/authorize"
   const RESPONSE_TYPE="token"
   const [token,setToken]=useState("")
+  const [x, setX] = useState()
+  const [y, setY] = useState()
+  const ref = useRef(null);
+  const[width,setWidth]=useState()
+  const[height,setHeight]=useState()
+  let styleAppDiv = {
+    backgroundColor: `rgb(${x},${y},${x-y}`,
+    width:"100%",
+    height:"100%"
+  }
+
+  useLayoutEffect(() => {
+    setWidth(ref.current.offsetWidth);
+    setHeight(ref.current.offsetHeight);
+    //console.log(width,height)
+  });
+
+  useEffect(() => {
+      const update = (e) => {
+        setX(e.x)
+        setY(e.y)
+      }
+      console.log(width,height)
+      window.addEventListener('mousemove', update)
+      window.addEventListener('touchmove', update)
+      return () => {
+        window.removeEventListener('mousemove', update)
+        window.removeEventListener('touchmove', update)
+      }
+    },
+    [setX, setY]
+  )
 
   useEffect(()=>{
     const hash=window.location.hash
@@ -22,13 +56,16 @@ function App() {
     }
 
   },[])
-  
+  const logout=()=>{
+    setToken("")
+    window.localStorage.removeItem("token")
+  }
 
   return (
-    <div class="App">
+    <div class="App" style={styleAppDiv} ref={ref}>
       <h1 id="p1">Modia</h1>
-      {!token?<a href={`${AUTH_ENDPOINT}?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=${RESPONSE_TYPE}`}>Login to Spotify</a>:<button>Logout</button>}
-      
+      {!token?<a href={`${AUTH_ENDPOINT}?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=${RESPONSE_TYPE}`}>Login to Spotify</a>:<button onClick={logout}>Logout</button>}
+      <h1>{`x: ${x}; y: ${y};`}{width};{height}</h1>
 
     </div>
   );
