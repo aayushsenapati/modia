4c1ea63f028a97597a2fe27ccc88202d8c036453
aayushsenapati <92628133+aayushsenapati@users.noreply.github.com>
2022-10-23 18:44:58 +0530

    idk

--- src/App.js
+++ src/App.js
@@ -4,6 +4,15 @@ import "./App.css";
 import { useEffect, useState, useRef, useLayoutEffect } from "react";
 import axios from "axios";
 
+export default function App(){
+  useEffect(()=>{
+    const hash=window.location.hash
+    
+  })
+}
+
+
+
 function Login() {
   const handleClick = async () => {
     const client_id = "54c6d6c8a6f347ba9c4d03f1d5be8f3c";
