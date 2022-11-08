b31b089934db315767daa403a2315a13bc19f06f
aayushsenapati <92628133+aayushsenapati@users.noreply.github.com>
2022-11-08 23:15:30 +0530

    server stuff

---
+++ server/index.js
@@ -0,0 +1,24 @@
+//import app from "./server.js";
+import mongoose from "mongoose";
+import * as dotenv from "dotenv";
+dotenv.config();
+console.log(process.env.MODIA_DB_URI)
+
+
+
+//const port = process.env.PORT || 8000;
+
+mongoose
+    .connect(process.env.MODIA_DB_URI, {
+        //   these are options to ensure that the connection is done properly
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useCreateIndex: true,
+    })
+    .then(() => {
+        console.log("Successfully connected to MongoDB Atlas!");
+    })
+    .catch((error) => {
+        console.log("Unable to connect to MongoDB Atlas!");
+        console.error(error);
+    });
