11d4e88f6909f6ec142911f8236a3c270c62df44
AayushNagarr <aayuanku@gmail.com>
2022-11-09 23:02:38 +0530

    commented which region is having which mood

--- src/MoodPalette.js
+++ src/MoodPalette.js
@@ -38,22 +38,22 @@ function MoodPalette(props) {
       top: "0px",
     };
 
-`    if (y < -(height / width) * x + height && y > (height / width) * x) {
+    if (y < -(height / width) * x + height && y > (height / width) * x) {
         styleMood.backgroundColor = "#3144b7"; //blue
         styleMood.color = "#9cf0e1";
-      } //blue
+      } //blue//mellow
       if (y < (height / width) * x && y < -(height / width) * x + height) {
         styleMood.backgroundColor = "#8bf677"; // green
         styleMood.color = "#7f14ff";
-      } //green
+      } //green//Somber
       if (y > -(height / width) * x + height && y < (height / width) * x) {
         styleMood.backgroundColor = "#fff914"; //yellow
         styleMood.color = "#d9006a";
-      } //yellow
+      } //yellow//upbeat
       if (y > -(height / width) * x + height && y > (height / width) * x) {
         styleMood.backgroundColor = "#ff4e2a"; //red
         styleMood.color = "#290740";
-      } //red`
+      } //red//joyous
         
     useEffect(() => {
       setWidth(ref.current.offsetWidth);
