5c7b642996d7c437f4f4569f43258ce30f7e9ea2
AayushNagarr <aayuanku@gmail.com>
2022-11-20 22:09:03 +0530

    playlist render

--- src/Playlist.js
+++ src/Playlist.js
@@ -1,29 +1,46 @@
+
 import "./App.css";
 import axios from "axios";
-import {useState,useEffect} from "react";
+import { useState, useEffect } from "react";
+
+const PlayBox = styled.div`
+    display : inline-block;
+    background-color : ${props.bgColor};
+    color-${props.color}
+    border-radius : 20px;
+    padding : 5px;
+    margin : 
+
+    `;
+
 
 function Playlist(props) {
-    const [data,setData]=useState()
-
-
-
-        const getPlay = async () =>
-        {
-            const { data } = await axios.post("http://127.0.0.1:5000/api/getPlay", {
-                headers: {
-                    "Content-Type": "application/json",
-                },
-                _id: props.ud.id,
-            });
-            console.log(data);  
-            //  setData(data)
-            
-        }
-        getPlay()
+    const [data, setData] = useState()
+    useEffect(() =>{
+        let isMounted = true
     
+
+
+    const getPlay = async () => {
+        const { data } = await axios.post("http://127.0.0.1:5000/api/getPlay", {
+            headers: {
+                "Content-Type": "application/json",
+            },
+            _id: props.ud.id,
+        });
+        console.log(data[0]);
+        if(isMounted)
+            setData(data)
+
+    }
+
+    if (!data)
+        getPlay()
+    })
+
     return (
         <>
-            
+            {data?data[0].playlists.map(playlist=><h5>{playlist.playName}</h5>):"N/A"}
         </>
     );
 }
