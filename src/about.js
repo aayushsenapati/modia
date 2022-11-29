b3c2f0a1d85a92e91f59eeefe5401b0a22d904f0
AayushNagarr <aayuanku@gmail.com>
2022-11-29 22:41:49 +0530

    About modify

--- src/about.js
+++ src/about.js
@@ -1,12 +1,30 @@
 import "./App.css";
+import backg from "./webgang.jpeg"
+
 
 
 export function About() {
+    const NameStyle = {
+        fontSize : "1vw", color : "white"
+
+    }
     return(   
     <>
+        <img src={backg} alt="err" style={{width:'100vw',height:'auto', opacity : "0.5", position : "sticky", top : "1vw", backgroundColor : "white", zIndex : "1"}}/>  
 
-    <img src="webgang.jpeg" alt="err" style={{width:'100vw',height:'auto'}}/>
+
+    <div style = {{position : "relative", backgroundColor : "#202020", width : "100vw", height : "1000px", zIndex : "2"}}>
+        <h1 style = {{color : "white", textAlign : "center"}}>Devs</h1><hr style = {{color : "white"}}/>
+        <div style = {{display : "flex"}}>
+        <span style = {NameStyle}>Aadithya H Rao</span>
+        <span style = {NameStyle}>Aayush Nagar</span>
+        <span style = {NameStyle}>Aayush Senapati</span>
+        </div>
+
+
         
+
+    </div>
     </>
     )
 }
