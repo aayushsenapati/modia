996919491d92f774379e84b87ea6685967138b1e
AayushNagarr <aayuanku@gmail.com>
2022-11-29 23:48:01 +0530

    wowow

--- src/about.js
+++ src/about.js
@@ -5,13 +5,15 @@ import backg from "./webgang.jpeg"
 
 export function About() {
     const NameStyle = {
-        fontSize : "1vw",
+        fontSize : "1.5vw",
         backgroundColor : "white",
         borderRadius : "10px",
         color : "black",
         padding : "0.6vw",
         marginLeft : "5vw",
         marginRight : "5vw"
+
+
 
     }
     return(   
@@ -22,9 +24,28 @@ export function About() {
     <div style = {{position : "relative", backgroundColor : "#202020", width : "100vw", height : "1000px", zIndex : "2", borderRadius : "10px"}}>
         <h1 style = {{color : "white", textAlign : "center"}}>Devs</h1><hr style = {{color : "white"}}/>
         <div style = {{display : "flex", justifyContent : "center"}}>
-        <span style = {NameStyle}>Aadithya H Rao</span>
         <span style = {NameStyle}>Aayush Nagar</span>
         <span style = {NameStyle}>Aayush Senapati</span>
+        <span style = {NameStyle}>Aadithya H Rao</span>
+        </div>
+
+        <div style = {{margin : "10vw", fontSize : "2.4vw", color : "white", textAlign : "left"}}>
+            <ul>Modules utilized
+
+                <div style = {{fontSize : "1.4vw"}}><li>axios</li>
+                <li>cors</li>
+                <li>express</li>
+                <li>mongoose</li>
+                <li>nodemon</li>
+                <li>pure-react-carousel</li>
+                <li>react-bootstrap</li>
+                <li>react-router-dom</li></div>
+            </ul>
+       
+        </div>
+
+        <div style = {{fontSize : "3vw", margin : "2vw"}}>
+        Inspiration
         </div>
 
 
