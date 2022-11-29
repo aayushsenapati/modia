2b5d33e4134b244ce4d85b7eee0105003c1e5478
aayushsenapati <92628133+aayushsenapati@users.noreply.github.com>
2022-11-29 19:10:51 +0530

    laoal

--- src/Playlist.js
+++ src/Playlist.js
@@ -17,10 +17,9 @@ const PlayBox = styled.div`
 
 const PlayBoxParent = styled.div`
     display: flex;
-  flex-wrap: wrap;
-  justify-content: center;
-  align-items: flex-start;
-  
+    flex-wrap: wrap;
+    justify-content: center;
+    align-items: flex-start;
     `;
 
 
@@ -31,7 +30,7 @@ function Playlist(props) {
     useEffect(() => {
         let isMounted = true
 
-        
+
 
         const getPlay = async () => {
             const { data } = await axios.post("http://127.0.0.1:5000/api/getPlay", {
@@ -50,23 +49,23 @@ function Playlist(props) {
             getPlay()
     })
 
-    
 
-    
+
+
     return (
 
-    <PlayBoxParent>
-        {data ? data[0].playlists.map((playlist) => (
-                <div key={playlist.playName} className='collapsed' style={{zIndex : "1", border : "2px solid", borderColor :"white", color : "white",borderRadius : '10px',padding : '5px',margin : '20px',backgroundColor : 'black',color : 'white',width : '20vw',fontSize : '3vh',height:'auto'}} 
-                    onClick={(e)=>{e.currentTarget.lastChild.style.display=="none"? e.currentTarget.lastChild.style.display="inline":e.currentTarget.lastChild.style.display="none"}}>
-                    <h5 style={{fontSize:"2vw"}} key={playlist.playName+" heading"}>{playlist.playName}</h5>
-                    <div key={playlist.playName+" ul"} style={{display:"none"}}>
+        <PlayBoxParent>
+            {data ? data[0].playlists.map((playlist) => (
+                <div key={playlist.playName} className='collapsed' style={{ zIndex: "1", border: "2px solid", borderColor: "white", color: "white", borderRadius: '10px', padding: '5px', margin: '20px', backgroundColor: 'black', color: 'white', width: '20vw', fontSize: '3vh', height: 'auto' }}
+                    onClick={(e) => { e.currentTarget.lastChild.style.display == "none" ? e.currentTarget.lastChild.style.display = "inline" : e.currentTarget.lastChild.style.display = "none" }}>
+                    <h5 style={{ fontSize: "2vw" }} key={playlist.playName + " heading"}>{playlist.playName}</h5>
+                    <div key={playlist.playName + " ul"} style={{ display: "none" }}>
                         {playlist.tracks.map((song) => (
-                            <h6 style={{fontSize:"1.5vw"}} key={song.track.name}>{song.track.name}</h6>
+                            <h6 style={{ fontSize: "1.5vw" }} key={song.track.name}>{song.track.name}</h6>
                         ))}
                     </div>
                 </div>
-        )) : <h6>No Playlist</h6>}</PlayBoxParent>
+            )) : <h6>No Playlist</h6>}</PlayBoxParent>
     );
 }
 
--- src/Recommend.js
+++ src/Recommend.js
@@ -9,14 +9,15 @@ import {
 } from "react";
 import axios from "axios";
 import styled from "styled-components";
-import React from 'react';  
-import {Button} from "react-bootstrap";
+import React from 'react';
+import { Button } from "react-bootstrap";
 import { CarouselProvider, Slider, Slide, ButtonBack, ButtonNext } from 'pure-react-carousel';
 import 'pure-react-carousel/dist/react-carousel.es.css';
-import { Routes, Route, useNavigate } from "react-router-dom";
-import userEvent from "@testing-library/user-event";
+import {useNavigate } from "react-router-dom";
 import backArrow from "./back-button.svg"
 import nextArrow from "./next-button.svg"
+import { MDBInput } from 'mdb-react-ui-kit';
+import Form from 'react-bootstrap/Form';
 
 
 
@@ -34,7 +35,7 @@ function SlideProvide(props) {
       <div key={song.track.id}>
         <Slide
           index={song.index}
-          onDoubleClick ={(e) => {
+          onDoubleClick={(e) => {
             tempTracks.includes(song) ? tempTracks.splice(tempTracks.indexOf(song), 1) : tempTracks.push(song);
             e.currentTarget.style.transition = "background-color 0.5s ease,color 0.5s ease";
             if (e.currentTarget.style.backgroundColor == props.bgColor) {
@@ -46,13 +47,13 @@ function SlideProvide(props) {
               e.currentTarget.style.color = props.color;
             }
             props.slidePropFunc(tempTracks); console.log(tempTracks);
-          } }
+          }}
 
           style={{ backgroundColor: props.bgColor, border: "10px solid", borderColor: '#202020', borderRadius: "2.3vw" }}>
 
-          <img src={song.track.album.images[0].url} alt="image" style={{width:'90%',height:'auto',marginTop:'1vw',borderRadius:'1.5vw'}}></img>
-          <h4  style={{fontSize : '1.8vw',height:'3.6vw',marginTop:'0.3vw'}} >{song.track.name.length>30?song.track.name.slice(0,27)+'...':song.track.name}</h4>
-          <h5 style={{position:'absolute',marginLeft:'10px', fontSize : '1.3vw',marginTop:'0.5vw'}}>{song.track.artists[0].name}</h5>
+          <img src={song.track.album.images[0].url} alt="image" style={{ width: '90%', height: 'auto', marginTop: '1vw', borderRadius: '1.5vw' }}></img>
+          <h4 style={{ fontSize: '1.8vw', height: '3.6vw', marginTop: '0.3vw' }} >{song.track.name.length > 30 ? song.track.name.slice(0, 27) + '...' : song.track.name}</h4>
+          <h5 style={{ position: 'absolute', marginLeft: '10px', fontSize: '1.3vw', marginTop: '0.5vw' }}>{song.track.artists[0].name}</h5>
 
         </Slide>
       </div>
@@ -75,8 +76,8 @@ function SlideProvide(props) {
       <div style={{ background: "linear-gradient(to right,rgba(32,32,32,1) 0%,rgba(32,32,32,1) 30%, rgba(32,32,32,0.8)60%, rgba(32,32,32,0.1) 95%, rgba(32,32,32,0) 100%)", position: 'absolute', width: '40vw', height: '100%', top: '0' }}></div>
       <div style={{ background: "linear-gradient(to left,rgba(32,32,32,1) 0%,rgba(32,32,32,1) 30%, rgba(32,32,32,0.8)60%, rgba(32,32,32,0.1) 95%, rgba(32,32,32,0) 100%)", position: 'absolute', width: '40vw', height: '100%', top: '0', left: '60vw' }}></div>
 
-       <ButtonBack style={{background:'none',border:'none',height:'4vw',width:'4vw',position:'absolute',top:'50%',left:'32%'}}><img src={backArrow} style={{width:'4vw',height:'auto'}} alt="not found"/></ButtonBack>
-       <ButtonNext style={{background:'none',border:'none',height:'4vw',width:'4vw',position:'absolute',top:'50%',left:'62%'}}><img src={nextArrow} style={{width:'4vw',height:'auto'}} alt="Not Found"/></ButtonNext>
+      <ButtonBack style={{ background: 'none', border: 'none', height: '4vw', width: '4vw', position: 'absolute', top: '50%', left: '32%' }}><img src={backArrow} style={{ width: '4vw', height: 'auto' }} alt="not found" /></ButtonBack>
+      <ButtonNext style={{ background: 'none', border: 'none', height: '4vw', width: '4vw', position: 'absolute', top: '50%', left: '62%' }}><img src={nextArrow} style={{ width: '4vw', height: 'auto' }} alt="Not Found" /></ButtonNext>
     </CarouselProvider>)
 }
 
@@ -91,7 +92,7 @@ function Recommend(props) {
   const refInput = useRef(null)
   const [playArr, setPlayArr] = useState([])
   const [staged, setStaged] = useState(false)
-  const [reRender,setReRender]=useState(false)
+  const [reRender, setReRender] = useState(false)
   const idArray = [];
   let navigate = useNavigate();
 
@@ -113,7 +114,7 @@ function Recommend(props) {
       <>
         {playArr.map((song) =>
         (
-          <h6 style = {{color : "white"}}key={song.track.id}>{song.track.name}</h6>
+          <h6 style={{ color: "white" }} key={song.track.id}>{song.track.name}</h6>
         ))}
       </>)
   }
@@ -209,34 +210,33 @@ function Recommend(props) {
 
         return (
           <>
-            <div style={{ margin: "100px" , color : "white"}}>
-              <h6 style={{ marginTop: "0px", color : "white" }}>Valence:{props.valence}</h6>
-              <h6 style = {{color : "white"}}>Energy:{props.energy}</h6>
-              <h6 style = {{color : "white"}}>Color:{props.color}</h6>
-              <h6 style = {{color : "white"}}>BGColor:{props.bgColor}</h6>
+            <div style={{ margin: "100px", color: "white" }}>
+              <h6 style={{ marginTop: "0px", color: "white" }}>Valence:{props.valence}</h6>
+              <h6 style={{ color: "white" }}>Energy:{props.energy}</h6>
+              <h6 style={{ color: "white" }}>Color:{props.color}</h6>
+              <h6 style={{ color: "white" }}>BGColor:{props.bgColor}</h6>
             </div>
 
             <div>
               <MemoSlideProvide color={props.color} bgColor={props.bgColor} objectArray={objectArray} slidePropFunc={slidePropFunc} />
             </div>
 
-              <div style={{ margin: "100px" }}>
-                <input id="playInput" type="text" placeholder = "Enter playlist name" ref={refInput} />
-                <Button variant="outline-light" style = {{margin : "5px"}}onClick={() => { setPlayName(refInput.current.value);setStaged(true);reRender?setReRender(false):setReRender(true)}}>Stage Playlist</Button>{' '}
-                {/*<button onClick={() => { setPlayName(refInput.current.value);setStaged(true)}}>Stage Playlist</button>*/}
-                
-                {staged?<div>
+            <div style={{ margin: "100px" }}>
+              <MDBInput id="playInput" type="text" label="Enter playlist name" ref={refInput} style={{width:"15vw"}}/>
+              <Button variant="outline-light" style={{ margin: "5px" }} onClick={() => { setPlayName(refInput.current.value); setStaged(true); reRender ? setReRender(false) : setReRender(true) }}>Stage Playlist</Button>{' '}
 
-                <h3 style = {{color : "white"}}>{playName}</h3>
-                <hr style = {{color : "white"}}/>
-                   {dispPlay()}
-                   <hr style = {{color : "white"}}/>
-                   </div>:<></>
-                   }
-                
-                {staged?<Button variant="outline-secondary" onClick={() => {playArr.length&&refInput.current.value.length ? uploadPlay() : window.alert("Select some songs or enter playlist name!");}}>Upload Playlist</Button>:<></>}
-                
-              </div>
+              {staged ? <div>
+
+                <h3 style={{ color: "white" }}>{playName}</h3>
+                <hr style={{ color: "white" }} />
+                {dispPlay()}
+                <hr style={{ color: "white" }} />
+              </div> : <></>
+              }
+
+              {staged ? <Button variant="outline-secondary" onClick={() => { playArr.length && refInput.current.value.length ? uploadPlay() : window.alert("Select some songs or enter playlist name!"); }}>Upload Playlist</Button> : <></>}
+
+            </div>
 
           </>
 
